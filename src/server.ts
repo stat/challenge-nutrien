acb1736763e79806c5720aa2d8324db6055a16e1
Chris Gerber <csgerber@gmail.com>
2024-05-15 00:37:34 -0700

    loading csv data into sqlite

--- src/server.ts
+++ src/server.ts
@@ -58,16 +58,17 @@ const __dirname = path.dirname(__filename);
  * @param {string} name - the name of the table to create
  * @returns {Promise<void>}
  */
-function createTable(name:string):Promise<void> {
-  return executeQuery(`
-    CREATE TABLE IF NOT EXISTS ${name}
+function createTable(db:sql.Database, name:string):Promise<void> {
+  return executeQuery(
+    db,
+    `CREATE TABLE IF NOT EXISTS ${name}
     (
       attribute VARCHAR(64),
       commodity VARCHAR(64),
       commodity_type VARCHAR(64),
       units VARCHAR(64),
-      year TIMESTAMP,
       year_type VARCHAR(64),
+      year VARCHAR(64),
       value NUMERICAL
     )`
   );
@@ -88,9 +89,25 @@ export function csvStream(path:string) {
  * @param {string} query - the query to execute
  * @returns {Promise<void>}
  */
-function executeQuery(query: string): Promise<void> {
+function executeQuery(db:sql.Database, query: string): Promise<void> {
   return new Promise((resolve, reject) => {
     db.exec(query, function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ * @param {string} query - the query to execute
+ * @returns {Promise<void>}
+ */
+function runQuery(db:sql.Database, query: string, ...args:any): Promise<void> {
+  return new Promise((resolve, reject) => {
+    db.run(query, args, function(err) {
       if (err) {
         reject(err);
       } else {
@@ -157,24 +174,54 @@ function expressServe(app:Express):void {
   });
 }
 
+function insertRow(db:sql.Database, table:string, data:Array<any>):Promise<void> {
+  if (!(data[6] instanceof Number)) {
+    data[6] = Number(data[6]);
+  }
+
+  return runQuery(
+    db,
+    `INSERT INTO ${table}
+    (
+      attribute,
+      commodity,
+      commodity_type,
+      units,
+      year_type,
+      year,
+      value
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?)`,
+    ...data
+  )
+}
+
 /**
  * @param {string} path - the location of the csv projection data
+ * @param {sql.Database} db - the sqlite3 db
+ * @param {string} table - the name of the table to load into
  * @returns {Promise<Array<Projection>>} the contents of the csv file
  */
-function loadData(path:string, db:sql.Database, table:string):Promise<Array<Projection>> {
-  return new Promise<Array<Projection>>((resolve, reject) => {
-    let items:Array<Projection> = [];
+function loadData(path:string, db:sql.Database, table:string):Promise<Number> {
+  return new Promise<Number>((resolve, reject) => {
+    let count = 0;
 
     csvStream(path)
       // .on('data', (row:Map<string, any>) => {
       .on('data', (row:Array<string>) => {
-
+        insertRow(db, table, row)
+          .then(() => {
+            count++;
+          })
+          .catch((e) => {
+            reject(e);
+          });
       })
       .on("close", function (err:Error) {
         reject(err);
       })
       .on("end", function () {
-        resolve(items);
+        resolve(count);
       });
   });
 }
@@ -186,14 +233,17 @@ function loadData(path:string, db:sql.Database, table:string):Promise<Array<Proj
 async function initialize() {
   // create the table
   try {
-    await createTable(dbTableName);
+    console.log("creating in memory table");
+    await createTable(db, dbTableName);
   } catch(e) {
     console.error(e);
   }
 
   // load data into the table
   try {
-    await loadData(dataPath, db, dbTableName);
+    console.log("loading data...");
+    const count = await loadData(dataPath, db, dbTableName);
+    console.log(`loaded ${count} rows`);
   } catch(e) {
     console.error(e);
   }
@@ -213,45 +263,3 @@ export async function serve() {
   await initialize();
   expressServe(app);
 }
-//
-// Start
-//
-
-// await serve();
-
-// // initialize
-// await initialize();
-
-// // serve
-// expressServe(app);
-
-// load
-// const rs = fs.createReadStream(envDataPath);
-
-//
-// db.serialize(() => {
-//   rs.pipe(csv())
-//     .on('headers', (headers) => {
-//       console.log(headers);
-
-//       db.run(`CREATE TABLE IF NOT EXISTS ${dbTableName}`, (err) => {
-
-//         if (err) {
-//           console.error('Error creating table:', err.message);
-//           rs.destroy();
-//         }
-//       });
-
-//       console.log(`Table '${dbTableName}' created successfully.`);
-//     })
-//     .on('data', (data) => {
-//       // console.log(data);
-//     })
-//     .on("close", function (err:Error) {
-//       console.log("Unsuccessfully loaded data");
-//     })
-//     .on("end", function () {
-//       console.log("Successfully loaded data");
-//     });
-// });
-
